0d4579e0c18d8fde03b7ee85da62fe5cc0c2f408
keenwang <keenwang@zego.im>
2022-03-20 00:11:56 +0800

    🐛  解决注册副作用函数后访问无关属性时 activeEffect 指向错误指向上一个。

--- demo/index.ts
+++ demo/index.ts
@@ -1,2 +1,30 @@
-import {main} from './reactive'
-main()
+import { registerEffect, reactive } from './reactive'
+// 原始数据
+const originData: any = { text: "Hello World!", title: "", showText: true }
+// 原始数据代理
+const data = reactive(originData)
+const textEffect = () => {
+    // 三元表达式 根据data.showText的值出现分支切换
+    // const text = data.showText ? data.text : "hide text"
+    const text = data.text
+    console.log('text change!', text);
+    document.getElementById("app")!.innerText = text
+}
+textEffect.label = "textEffect"
+registerEffect(textEffect, textEffect.label)
+const titleEffect = () => {
+    const title = data.title
+    console.log('title change!', title);
+    document.getElementById("title")!.innerText = title
+}
+titleEffect.label = "titleEffect"
+registerEffect(titleEffect, titleEffect.label)
+
+setTimeout(() => {
+    // data.showText = false
+    data.text = "Hello Keen!" + Date.now()
+    // setInterval(() => {
+    //     data.text = "Hello Keen!" + Date.now()
+    // }, 1000)
+    data.title = "Title"
+}, 1000);
--- demo/reactive/index.ts
+++ demo/reactive/index.ts
@@ -1,70 +1,72 @@
-export function main() {
-    // 存储副作用函数的集合： WeakMap 将闭包作用域中的对象强引用，避免影响垃圾回收。
-    const bucket: WeakMap<object, Map<string | Symbol, Set<Function>>> = new WeakMap()
-    // 原始数据
-    const originData: any = { text: "Hello World!", h1: "", showText: false }
-    // 原始数据代理
-    const data = new Proxy(originData, {
+
+/**
+ *  存储副作用函数的集合： WeakMap 将闭包作用域中的对象强引用，避免影响垃圾回收。
+ */
+const bucket: WeakMap<object, Map<string | Symbol, Set<Function>>> = new WeakMap()
+/** 数据响应式 */
+export function reactive<T extends object>(origin: T): T {
+    return new Proxy(origin, {
         get(target, key) {
             trace(target, key)
+            // @ts-ignore 
             return target[key]
         },
         set(target, key, value) {
-            // 设置新值
+            // @ts-ignore 设置新值
             target[key] = value
             trigger(target, key)
             return true
         }
     })
-
-    /** 追踪属性 */
-    function trace(target: any, key: string | symbol) {
-        if (!activeEffect) return
-        let depsMap = bucket.get(target)
-        if (!depsMap) {
-            depsMap = new Map()
-            bucket.set(target, depsMap)
-        }
-        let deps = depsMap.get(key)
-        if (!deps) {
-            deps = new Set()
-            depsMap.set(key, deps)
-        }
-        
-        // 将副作用函数收集到目标对象对应的属性下面的依赖集合 target.key.deps
-        deps.add(activeEffect)
-        // TODO: 何时去掉这里的 ativeEffect 避免读取别的属性时把这个方法添加到依赖
+}
+/** 追踪属性 */
+function trace(target: any, key: string | symbol) {
+    console.warn('[trace] Bind key and effect: ', key, activeEffect.label);
+    if (!activeEffect) return
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        bucket.set(target, depsMap)
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+        deps = new Set()
+        depsMap.set(key, deps)
     }
 
-    function trigger(target: any, key: string | symbol) {
-        // 获取副作用函数
-        const depMap = bucket.get(target)
-        if (!depMap) return
-        const effects = depMap.get(key)
-        effects && effects.forEach(fn => fn())
-    }
+    // 将副作用函数收集到目标对象对应的属性下面的依赖集合 target.key.deps
+    deps.add(activeEffect)
+    // TODO: 何时去掉这里的 ativeEffect 避免读取别的属性时把这个方法添加到依赖
+}
+/** 触发副作用函数 */
+function trigger(target: any, key: string | symbol) {
+    // 获取副作用函数
+    const depMap = bucket.get(target)
+    if (!depMap) return
+    const effects = depMap.get(key)
+    effects && effects.forEach(fn => fn())
+}
 
-    // 用一个全局变量存被注册的副作用函数
-    let activeEffect: Function | undefined
-    /*注册副作用函数*/
-    function registerEffect(fn: Function) {
-        // 保存副作用函数
-        activeEffect = fn
+/**用一个全局变量存被注册的副作用函数*/
+let activeEffect: any | undefined
+/*注册副作用函数*/
+export function registerEffect(fn: Function, label?: string) {
+    // A方式: 原本没有闭包的情况下，activeEffect 在读取变量时没有及时修改，导致出现 effect 挂在错误的 key 上
+    //  set() => fn() => get() => 将key和当前activeEffect绑定，此时的activeEffect指向的可能是错的。
+    /*
+    console.warn('[effectFn] activeEffect change:', label);
+    activeEffect = fn;
+    fn()
+    */
+
+    // B方式: 在每次执行副作用函数时，都重新设置 activeEffect ，确保get的时候拿到的是正确的 activeEffect。
+    // set() => effectFn() => 修改activeEffect指向effectFn => fn() => get() => 将key和当前activeEffect绑定
+    const effectFn = () => {
+        console.warn('[effectFn] activeEffect change:', effectFn.label);
+        activeEffect = effectFn;
         fn()
     }
-    registerEffect(() => {
-        console.log('text change!', data.text);
-        document.getElementById("app")!.innerText = data.showText ? data.text : ""
-    })
-    registerEffect(() => {
-        console.log('title change!', data.h1);
-        document.getElementById("title")!.innerText = data.h1
-    })
-
-    setTimeout(() => {
-        data.text = "Hello Keen!"
-        data.h1 = "Title"
-    }, 1000);
-
-
+    effectFn.label = label
+    effectFn()
 }
+
